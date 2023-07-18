8a1e4d791a6e4199a5cba9daf2d69f385587605e
Mangesh Dalvi <84315964+mangesh1599@users.noreply.github.com>
2023-07-18 19:31:44 +0530

    Create IterateThroughAllElementsInLinkedListStartingAtTheSpecifiedPosition.java

---
+++ IterateThroughAllElementsInLinkedListStartingAtTheSpecifiedPosition.java
@@ -0,0 +1,39 @@
+Write a program to iterate through all elements in a linked list starting at the specified position
+
+
+
+
+  import java.util.*;
+public class MyClass 
+{
+	//StartOfMainMethod
+	public static void main(String[] args) 
+	{
+		LinkedList<Integer> list1=new LinkedList<Integer>();
+		list1.add(4);
+		list1.add(6);
+		list1.add(3);
+		list1.add(9);
+		list1.add(7);
+		int pos=1;
+		myCode(list1,pos);
+	}
+	//EndOfMainMethod
+	public static void myCode(LinkedList<Integer> list1,int pos) 
+	{
+		//write code here
+		for(int data:list1)
+		{
+			System.out.println(data);
+		}
+	}
+}
+
+
+
+Output-
+        4
+        6
+        3
+        9
+        7
